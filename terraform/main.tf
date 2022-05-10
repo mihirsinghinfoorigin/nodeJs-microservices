6c9e5ae45940d727c4764777be4bd50e80cb13f6
Mihir Singh <mihir.singh@infoorigin.com>
2022-05-10 17:32:37 +0530

    add terraform

---
+++ terraform/main.tf
@@ -0,0 +1,50 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+resource "azurerm_resource_group" "resource_group" {
+  name     = "${var.resource_group}_${var.environment}"
+  location = var.location
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_kubernetes_cluster" "terraform-k8s" {
+  name                = "${var.cluster_name}_${var.environment}"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  dns_prefix          = var.dns_prefix
+
+  linux_profile {
+    admin_username = "ubuntu"
+
+    ssh_key {
+      key_data = file(var.ssh_public_key)
+    }
+  }
+
+  default_node_pool {
+    name            = "agentpool"
+    node_count      = var.node_count
+    vm_size         = "standard_b2ms"
+  }
+
+  service_principal {
+    client_id     = var.client_id
+    client_secret = var.client_secret
+  }
+
+  tags = {
+    Environment = var.environment
+  }
+}
