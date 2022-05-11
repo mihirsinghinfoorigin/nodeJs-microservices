c44bfdc1c1ae75feecde0c84c2b7bcc1b36ed454
Mihir Singh <mihir.singh@infoorigin.com>
2022-05-11 18:11:46 +0530

    modified main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,3 @@
-# Configure the Azure provider
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
-    }
-  }
-
-  required_version = ">= 1.1.0"
-}
-
 resource "azurerm_resource_group" "resource_group" {
   name     = "${var.resource_group}_${var.environment}"
   location = var.location
