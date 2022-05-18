176a97391c283ffac8b95eae62e1c71ce69c1ae3
Mihir Singh <mihir.singh@infoorigin.com>
2022-05-18 21:54:19 +0530

    keyvault added in docker

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,12 +3,26 @@
 const properties = require('../package.json');
 const distance = require('../service/distance');
 
+const az_identity = require('@azure/identity');
+const az_secret = require('@azure/keyvault-secrets');
+
+const credentials = new az_identity.DefaultAzureCredential();
+const client = new az_secret.SecretClient('https://zipcodeapikeyvault.vault.azure.net/', credentials);
+
+const request = require('request');
+var apiKey = "";
+client.getSecret(process.env.ZIPCODE_API_KEY).then(res => {
+    apiKey = res.value;
+}
+).catch(err => { console.log(err);});
+
 var controllers = {
     about: (req, res) => {
         var aboutInfo = {
             author: 'Mihir Singh',
             name: properties.name,
             version: properties.version,
+            apiKey: apiKey
         }
         res.json(aboutInfo);
     },
