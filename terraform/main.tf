4c61c66cb23f64b662d5d04ff8a19131f3f4f89e
Mihir Singh <mihir.singh@infoorigin.com>
2022-05-20 14:52:56 +0530

    sql endpoints added

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,8 +36,8 @@ resource "azurerm_virtual_network" "k8s_vnet" {
 
 resource "azurerm_subnet" "k8s-nodepool" {
   name                 = "default"
-  virtual_network_name = azurerm_virtual_network.example.name
-  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.k8s_vnet.name
+  resource_group_name  = azurerm_resource_group.resource_group.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
