c4495a0d428d7386b4fafd753a04d81c32872a25
Mihir Singh <mihir.singh@infoorigin.com>
2022-05-20 14:30:58 +0530

    sql endpoints added

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,16 +34,23 @@ resource "azurerm_virtual_network" "k8s_vnet" {
   }
 }
 
+resource "azurerm_subnet" "k8s-nodepool" {
+  name                 = "default"
+  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = azurerm_resource_group.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
 resource "azurerm_subnet" "k8s_vnet_subnet" {
     name             = "k8s_vnet_subnet"
     virtual_network_name = azurerm_virtual_network.k8s_vnet.name
     resource_group_name = azurerm_resource_group.resource_group.name
     address_prefixes     = ["10.0.0.0/24"]
-    service_endpoints    = ["Microsoft.KeyVault"]
+    service_endpoints    = ["Microsoft.KeyVault","Microsoft.Sql"]
 }
 
 resource "azurerm_api_management" "k8s_apim" {
-  name                = "k8s-apim-ms007"
+  name                = "k8s-apim-ms09"
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
   publisher_name      = "InfoOrigin"
@@ -55,18 +62,6 @@ resource "azurerm_api_management" "k8s_apim" {
 
   virtual_network_configuration {
       subnet_id = azurerm_subnet.k8s_vnet_subnet.id
-  }
-
-  policy {
-    xml_content = <<XML
-    <policies>
-      <inbound />
-      <backend />
-      <outbound />
-      <on-error />
-    </policies>
-XML
-
   }
 }
 
