3a3781d6c53b1a134fae5dd96812fee3e0084d02
Mihir Singh <mihir.singh@infoorigin.com>
2022-05-20 15:03:53 +0530

    nsg updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,6 +20,22 @@ resource "azurerm_network_security_group" "vnet_security_group" {
   name                = "k8stest_security_group"
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
+
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = var.environment
+  }
 }
 
 resource "azurerm_virtual_network" "k8s_vnet" {
